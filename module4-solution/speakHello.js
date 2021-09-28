9677b455e42e39ba43a0bdb68185dedd216205c1
RickyGhe <richighezzi2002@gmail.com>
2021-09-28 17:34:04 +0200

    module4-solution

---
+++ module4-solution/speakHello.js
@@ -0,0 +1,17 @@
+
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
+=======
+var helloSpeaker = {
+    speakWord : "Hello ",
+    speak : function(name) {
+        console.log(this.speakWord + " " + name);
+    } 
+}
